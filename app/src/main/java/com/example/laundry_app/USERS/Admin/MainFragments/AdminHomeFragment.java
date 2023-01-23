4c9360bca99e04dd986af00d62b9805898864e5f
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-01-23 13:55:37 +0800

    update: gark

--- app/src/main/java/com/example/laundry_app/USERS/Admin/MainFragments/AdminHomeFragment.java
+++ app/src/main/java/com/example/laundry_app/USERS/Admin/MainFragments/AdminHomeFragment.java
@@ -37,6 +37,9 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 //import com.github.mikephil.charting.data.BarEntry;
 //import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -44,6 +47,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
 
@@ -58,7 +62,7 @@ public class AdminHomeFragment extends Fragment {
     // ============================== COMPONENTS ============================== //
     // ============================== COMPONENTS ============================== //
 
-    TextView btnNoOfBookings, btnNoOfPickup, btnDailyIncome, btnMonthlyIncome;
+    TextView btnNoOfBookings, btnNoOfPickup, btnDailyIncome, btnMonthlyIncome, txtChartTitleStart,txtChartTitleEnd;
 
 
     // ============================== VARIABLES ============================== //
@@ -69,6 +73,7 @@ public class AdminHomeFragment extends Fragment {
     ArrayList dataX = new ArrayList<>();
     ArrayList dataY = new ArrayList<>();
     int entry1 = 10, entry2 = 5, entry3 = 7, entry4 = 2, entry5=4, entry6=6, entry7=17;
+    String barDescription = "";
     int numberOfBookings1;
     String token, finalToken;
     int totalBookings = 0;
@@ -83,6 +88,7 @@ public class AdminHomeFragment extends Fragment {
     AdminHomeModel adminHomeModel;
     AdminDashboard adminDashboard;
 
+    BarChart barChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -97,7 +103,9 @@ public class AdminHomeFragment extends Fragment {
         btnNoOfPickup = root.findViewById(R.id.txt_display_number_of_pickup);
         btnDailyIncome = root.findViewById(R.id.txt_display_total_of_daily_booking);
         btnMonthlyIncome = root.findViewById(R.id.txt_display_monthly_income);
-        BarChart barChart = root.findViewById(R.id.bar_chart);
+        txtChartTitleStart = root.findViewById(R.id.bar_chart_start_date);
+        txtChartTitleEnd = root.findViewById(R.id.bar_chart_end_date);
+        barChart = root.findViewById(R.id.bar_chart);
 
         adminHomeInterface = retrofit.create(AdminHomeInterface.class);
         bookingInterface = retrofit.create(BookingInterface.class);
@@ -110,6 +118,9 @@ public class AdminHomeFragment extends Fragment {
 
         getDataFromActivityAdmin();
 //        getAdminHomeInfo();
+        btnMonthlyIncome.setText("0");
+        monthlyIncom = 0;
+        totalPickups = 0;
         getAllBookings();
 
 
@@ -117,20 +128,9 @@ public class AdminHomeFragment extends Fragment {
 
         // ============================== Calling Methods ============================== //
 
-        getData();
 
         // ============================== Bar View ============================== //
 
-        BarDataSet barDataSet = new BarDataSet(barArrayList, "Sales");
-        BarData barData = new BarData(barDataSet);
-        barChart.setData(barData);
-        barDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
-        barDataSet.setValueTextColor(Color.BLACK);
-//        barDataSet.setValueTextSize(16f);
-        barChart.getDescription().setEnabled(true);
-        Description description = barChart.getDescription();
-        description.setText("Jan 08          Jan 09           Jan 10 " +
-                "         Jan 11          Jan 12          Jan 13           Jan 14    ");
 
 
         return root;
@@ -149,6 +149,17 @@ public class AdminHomeFragment extends Fragment {
         barArrayList.add(new BarEntry(6f, entry5));
         barArrayList.add(new BarEntry(7f, entry6));
         barArrayList.add(new BarEntry(8f, entry7));
+
+
+        BarDataSet barDataSet = new BarDataSet(barArrayList, "Sales");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+//        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(true);
+        Description description = barChart.getDescription();
+        description.setText(barDescription);
     }
 
     private void getDataFromActivityAdmin(){
@@ -192,7 +203,6 @@ public class AdminHomeFragment extends Fragment {
 //            }
 //        });
 //    }
-
     private void getAllBookings(){
         finalToken = "Bearer " + token;
         Call<BookingsRequest> requestCall = bookingInterface.getBookings(finalToken);
@@ -207,6 +217,11 @@ public class AdminHomeFragment extends Fragment {
                 BookingsRequest result = response.body();
                 // data
                 bookings = result.getBookings();
+                try {
+                    setEntries(bookings);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
                 totalBookings = bookings.size();
                 btnNoOfBookings.setText(String.valueOf(totalBookings));
                 for(int i = 0; i < bookings.size(); i++){
@@ -252,6 +267,78 @@ public class AdminHomeFragment extends Fragment {
         });
     }
 
+    private void setEntries(ArrayList<BookingModel> bookings) throws ParseException {
+        //? get all days from today + 6
+        //? get bookings for that day
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+        Calendar cal = Calendar.getInstance();
+        String[] days = new String[7];
+        int delta = -cal.get(GregorianCalendar.DAY_OF_WEEK) + 2;
+        cal.add(Calendar.DAY_OF_MONTH, delta );
+        for (int i = 0; i < 7; i++)
+        {
+            days[i] = format.format(cal.getTime());
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        Date monday = new Date(days[0]);
+        Date  tuesday = new Date(days[1]);
+        Date wednesday = new Date(days[2]);
+        Date thursday = new Date(days[3]);
+        Date friday = new Date(days[4]);
+        Date saturday = new Date(days[5]);
+        Date sunday = new Date(days[6]);
+
+        SimpleDateFormat sdfTitle = new SimpleDateFormat("MMM dd, yyyy");
+        txtChartTitleStart.setText(sdfTitle.format(monday));
+        txtChartTitleEnd.setText(sdfTitle.format(sunday));
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd");
+        barDescription = sdf.format(monday) + "          " + sdf.format(tuesday) +"            " + sdf.format(wednesday)+
+                "         " + sdf.format(thursday)+"          " + sdf.format(friday)+"          " +  sdf.format(saturday)+"           " +sdf.format(sunday)+"    ";
+
+        Log.d("TEST_BOOKINGS", format.format(monday));
+        Log.d("TEST_BOOKINGS", format.format(tuesday));
+        Log.d("TEST_BOOKINGS", format.format(wednesday));
+        Log.d("TEST_BOOKINGS", format.format(thursday));
+        Log.d("TEST_BOOKINGS", format.format(friday));
+        Log.d("TEST_BOOKINGS", format.format(saturday));
+        Log.d("TEST_BOOKINGS", format.format(sunday));
+        ArrayList<BookingModel> bookingDay1 = getBookingsPerDay(bookings, format.format(monday));
+        ArrayList<BookingModel> bookingDay2 = getBookingsPerDay(bookings, format.format(tuesday));
+        ArrayList<BookingModel> bookingDay3 = getBookingsPerDay(bookings, format.format(wednesday));
+        ArrayList<BookingModel> bookingDay4 = getBookingsPerDay(bookings, format.format(thursday));
+        ArrayList<BookingModel> bookingDay5 = getBookingsPerDay(bookings, format.format(friday));
+        ArrayList<BookingModel> bookingDay6 = getBookingsPerDay(bookings, format.format(saturday));
+        ArrayList<BookingModel> bookingDay7 = getBookingsPerDay(bookings, format.format(sunday));
+        entry1 = getEntryValue(bookingDay1);
+        entry2 = getEntryValue(bookingDay2);
+        entry3 = getEntryValue(bookingDay3);
+        entry4 = getEntryValue(bookingDay4);
+        entry5 = getEntryValue(bookingDay5);
+        entry6 = getEntryValue(bookingDay6);
+        entry7 = getEntryValue(bookingDay7);
+
+        getData();
+    }
+
+
+    private int getEntryValue(ArrayList<BookingModel> bookings){
+        int value = 0;
+        for(BookingModel item: bookings){
+            value += item.getTotal();
+        }
+        return value;
+    }
+
+    private ArrayList<BookingModel> getBookingsPerDay(ArrayList<BookingModel> bookings, String dateString){
+        ArrayList<BookingModel> dayBookings = new ArrayList<BookingModel>();
+        for (BookingModel item: bookings){
+            if(item.getDate().contains(dateString)){
+                dayBookings.add(item);
+            }
+        }
+        return dayBookings;
+    }
 //    private void getAdminHomeInfo(){
 //
 //        finalToken = "Bearer " + token;
